e91adfcf3e09adfabede7cdc39d2b153d044223c
mate3241 <szabomatedenes@gmail.com>
2020-02-26 12:02:41 +0100

    buggy

--- game.js
+++ game.js
@@ -5,7 +5,23 @@ const generate2DArray = (n, m) => {
   }
   return arr;
 };
+const shiftPush = (array) => {
+  for (let i = 0; i < array.length; i++) {
 
+    array[i].shift();
+    array[i].push();
+
+  }
+}
+const fill2DArray = (matrix, filler) => {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== '*' && matrix[i][j] !== 'p'){
+      matrix[i][j] = filler;
+      }
+    }
+  }
+};
 
 const print2DArray = (matrix) => {
   for (let i = 0; i < matrix.length; i++) {
@@ -14,4 +30,12 @@ const print2DArray = (matrix) => {
     }
     console.log();
   }
+  console.log();
 };
+let array = generate2DArray(20, 20);
+array[2][3] = '#';
+setInterval(() => {
+  print2DArray(array);
+  shiftPush(array);
+  fill2DArray(array, '.');
+}, 1000);
